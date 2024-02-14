f3e94e0347c0b202a7291a2ce0a23b73fda80b01
Fredrik <91frerin@gafe.molndal.se>
2024-02-14 16:23:20 +0100

    initial commit

---
+++ src/test/java/com/fredrikkodar/blogwebservice/BlogWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredrikkodar.blogwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
